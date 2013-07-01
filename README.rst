33d0609870ba962a78bcfc795d1b841249a73679
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-07-01 17:32:22 -0400

    Import Sphinx theme for OpenStack
    This is a copy of the Sphinx theme for OpenStack developer
    documentation imported from the ceilometer repository. After
    this library is released, we will remove the copies of the
    theme from all of the other repositories.
    
    blueprint oslo.sphinx
    
    Change-Id: I20b6c224b8d8fbede354c2a28ad67eda3a4b9dac
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

---
+++ README.rst
@@ -0,0 +1,8 @@
+OpenStack Sphinx Extensions
+===========================
+
+Theme and extension support for Sphinx documentation from the
+OpenStack project.
+
+To use the theme, add ``'oslo.sphinx'`` to the ``extensions`` list in
+the ``conf.py`` file in your Sphinx project.
---
+++ oslo/sphinx/__init__.py
@@ -0,0 +1,39 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 New Dream Network, LLC (DreamHost)
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+
+def builder_inited(app):
+    theme_dir = os.path.join(os.path.dirname(__file__), 'theme')
+    app.info('Using openstack theme from %s' % theme_dir)
+    # Insert our theme directory at the front of the search path and
+    # force the theme setting to use the one in the package. This is
+    # done here, instead of in setup(), because conf.py is read after
+    # setup() runs, so if the conf contains these values the user
+    # values overwrite these. That's not bad for the theme, but it
+    # breaks the search path.
+    app.config.html_theme_path.insert(0, theme_dir)
+    # Set the theme name
+    app.config.html_theme = 'openstack'
+    # Re-initialize the builder, if it has the method for setting up
+    # the templates and theme.
+    if hasattr(app.builder, 'init_templates'):
+        app.builder.init_templates()
+
+
+def setup(app):
+    app.connect('builder-inited', builder_inited)
